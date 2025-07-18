58babd4436e74d6b170694d8cd02d1e1fa0ca8cf
Atharv-Mesh <auti.atharv12@gmail.com>
2025-07-18 12:26:56 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,11 +33,11 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Sample',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'CEPT University',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+            'images/20241122 online portfolio9.jpg',
           link: 'https://example.com',
         },
         {
